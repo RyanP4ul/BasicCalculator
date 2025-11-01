fbbf126cfa52bf5dc25f5a610b473c41146ac40a
Ryan Paul Espinola <118099403+RyanP4ul@users.noreply.github.com>
2025-11-01 08:36:38 +0800

    Add files via upload

---
+++ CalculatorPrivateAssembly/CalculatorPrivateAssembly/BasicComputation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorPrivateAssembly
+{
+    public class BasicComputation
+    {
+        public static float Addition(float num1, float num2)
+        {
+            return num1 + num2;
+        }
+
+        public static float Subtraction(float num1, float num2)
+        {
+            return num1 - num2;
+        }
+
+        public static float Multiplication(float num1, float num2)
+        {
+            return num1 * num2;
+        }
+
+        public static float Division(float num1, float num2)
+        {
+            if (num2 == 0)
+            {
+                return 0;
+            }
+
+            return num1 / num2;
+        }
+    }
+}
