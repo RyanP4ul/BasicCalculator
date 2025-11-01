ef2e7dd1209a2664befa4d4ed2641834ae4ba807
RyanP4ul <espinolaryanpaul2028@gmail.comd>
2025-11-01 08:32:06 +0800

    First And Last

---
+++ BasicCalculator/FrmBasicCalculator.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace BasicCalculator
+{
+    partial class FrmBasicCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbInput1 = new System.Windows.Forms.TextBox();
+            this.tbInput2 = new System.Windows.Forms.TextBox();
+            this.cbOperation = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.rtbTotal = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(59, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(370, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Basic Calculator";
+            // 
+            // tbInput1
+            // 
+            this.tbInput1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbInput1.Location = new System.Drawing.Point(22, 98);
+            this.tbInput1.Name = "tbInput1";
+            this.tbInput1.Size = new System.Drawing.Size(430, 44);
+            this.tbInput1.TabIndex = 1;
+            this.tbInput1.Text = "0";
+            // 
+            // tbInput2
+            // 
+            this.tbInput2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbInput2.Location = new System.Drawing.Point(22, 243);
+            this.tbInput2.Name = "tbInput2";
+            this.tbInput2.Size = new System.Drawing.Size(430, 44);
+            this.tbInput2.TabIndex = 2;
+            this.tbInput2.Text = "0";
+            // 
+            // cbOperation
+            // 
+            this.cbOperation.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbOperation.FormattingEnabled = true;
+            this.cbOperation.Location = new System.Drawing.Point(168, 171);
+            this.cbOperation.Name = "cbOperation";
+            this.cbOperation.Size = new System.Drawing.Size(140, 45);
+            this.cbOperation.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SteelBlue;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(156, 433);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 46);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Compute";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // rtbTotal
+            // 
+            this.rtbTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbTotal.Location = new System.Drawing.Point(22, 315);
+            this.rtbTotal.Name = "rtbTotal";
+            this.rtbTotal.Size = new System.Drawing.Size(430, 96);
+            this.rtbTotal.TabIndex = 5;
+            this.rtbTotal.Text = "";
+            // 
+            // FrmBasicCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SlateGray;
+            this.ClientSize = new System.Drawing.Size(491, 505);
+            this.Controls.Add(this.rtbTotal);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cbOperation);
+            this.Controls.Add(this.tbInput2);
+            this.Controls.Add(this.tbInput1);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmBasicCalculator";
+            this.Text = "FrmBasicCalculator";
+            this.Load += new System.EventHandler(this.FrmBasicCalculator_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbInput1;
+        private System.Windows.Forms.TextBox tbInput2;
+        private System.Windows.Forms.ComboBox cbOperation;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox rtbTotal;
+    }
+}
+
---
+++ BasicCalculator/FrmBasicCalculator.cs
@@ -0,0 +1,67 @@
+﻿using CalculatorPrivateAssembly;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BasicCalculator
+{
+    public partial class FrmBasicCalculator : Form
+    {
+        public FrmBasicCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmBasicCalculator_Load(object sender, EventArgs e)
+        {
+            var operations = new List<string> { "+", "-", "*", "/" };
+
+            foreach (var operation in operations) cbOperation.Items.Add(operation);
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var num1 = Convert.ToInt32(tbInput1.Text);
+                var num2 = Convert.ToInt32(tbInput2.Text);
+                var operation = cbOperation.SelectedItem.ToString();
+                var result = "None";
+
+                Console.WriteLine(operation);
+
+                switch (operation)
+                {
+                    case "+":
+                        result = Convert.ToString(BasicComputation.Addition(num1, num2));
+                        break;
+                    case "-":
+                        result = Convert.ToString(BasicComputation.Subtraction(num1, num2));
+                        break;
+                    case "*":
+                        result = Convert.ToString(BasicComputation.Division(num1, num2));
+                        break;
+                    case "/":
+                        result = Convert.ToString(BasicComputation.Multiplication(num1, num2));
+                        break;
+                }
+
+                MessageBox.Show($"Result: {result}");
+
+                rtbTotal.Text = "Total: \n" + result;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+    }
+}
